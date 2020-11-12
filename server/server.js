480263ecf34514f57e5909518f92f11f509b716e
Allan Assis <allan.andrade@g.globo>
2020-11-12 16:24:09 -0300

    Change to udp server instead of tcp

--- server/server.js
+++ server/server.js
@@ -1,71 +1,59 @@
 const net = require("net")
+var dgram = require("dgram");
+
+var server = dgram.createSocket("udp4");
 
 const PORT = "3000"
 
-const server = net.createServer(function (connection) {
-
+server.on("connect", function(){
     console.log("Endereço do cliente: " + connection.remoteAddress + ":" + connection.remotePort)
-
-    connection.on("connect", function (data) {
-        connection.write("data", data)
-    })
-
-    connection.on('error', function (err) {
-        console.log('client disconnected');
-        console.log('error', err);
-    });
-
-    connection.on("data", function (buffer) {
-        const data = buffer.toString()
-        const dataList = data.split("=")
-        const option = dataList[0]
-        switch (option) {
-            case "1":
-
-                const number = +dataList[1]
-                if (Number.isNaN(number)) {
-                    connection.write("error=Na opção 1 é necessário enviar um inteiro na msg")
-                }
-                else {
-                    connection.write("data=" + (++number))
-                }
-                break;
-            case "2":
-                var char = dataList[1].toString()
-                if (char.length !== 1) {
-                    connection.write("error=Na opção 2 é necessário enviar apenar um caracter")
-                }
-                else {
-                    var asciiNumber = char.charCodeAt()
-
-                    if (asciiNumber === char.toLowerCase().charCodeAt()) {
-                        connection.write("data=" + char.toUpperCase())
-                    }
-                    else {
-                        connection.write("data=" + char.toLowerCase())
-                    }
-                }
-            case "3":
-                const sentence = dataList[1].toString()
-                const invertedSentence = sentence.split("").reverse().join("")
-                connection.write("data=" + invertedSentence)
-            default:
-                break;
-        }
-    })
-
-    connection.on('end', function () {
-        console.log('client disconnected');
-    });
-
-    connection.on("close", function (hadError) {
-        console.log("Ocorreu algum erro de tranmissão", hadError)
-    })
 })
 
 
-server.listen(PORT, function () {
-    const serverAddress = server.address()
-    const adress = serverAddress.address + ":" + serverAddress.port
-    console.log("Servidor escutando no endereço ", adress)
+server.on("message", function(buffer, rinfo){
+    const data = buffer.toString()
+    const dataList = data.split("=")
+    const option = dataList[0]
+    console.log(data)
+    switch (option) {
+        case "1":
+
+            const number = +dataList[1]
+            if (Number.isNaN(number)) {
+                server.send("error=Na opção 1 é necessário enviar um inteiro na msg", rinfo.port, rinfo.address)
+            }
+            else {
+                server.send("data=" + (number + 1), rinfo.port, rinfo.address)
+            }
+            break;
+        case "2":
+            var char = dataList[1].toString()
+            if (char.length !== 1) {
+                server.send("error=Na opção 2 é necessário enviar apenar um caracter",rinfo.port, rinfo.address)
+            }
+            else {
+                var asciiNumber = char.charCodeAt()
+
+                if (asciiNumber === char.toLowerCase().charCodeAt()) {
+                    server.send("data=" + char.toUpperCase(), rinfo.port, rinfo.address)
+                }
+                else {
+                    server.send("data=" + char.toLowerCase(), rinfo.port, rinfo.address)
+                }
+            }
+        case "3":
+            const sentence = dataList[1].toString()
+            const invertedSentence = sentence.split("").reverse().join("")
+            server.send("data=" + invertedSentence, rinfo.port, rinfo.address)
+        default:
+            break;
+    }
 })
+
+server.on("listening", function(){
+    var address = server.address();
+    console.log("server listening " +
+        address.address + ":" + address.port);
+})
+
+server.bind(PORT)
